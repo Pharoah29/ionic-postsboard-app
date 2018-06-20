588b02e458d31b2990152bd89197706b12337e86
pharoah29 <pharoah29@gmail.com>
2018-06-20 21:06:24 +0300

    first update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { AuthProvider } from "../providers/auth/auth";
+import { AddNoteModalPage } from "../pages/add-note-modal/add-note-modal";
+import { Camera } from '@ionic-native/camera';
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage, LoginPage, AddNoteModalPage],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage, LoginPage, AddNoteModalPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AuthProvider,
+    Camera
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, ModalController, NavParams } from "ionic-angular";
+import { AddNoteModalPage } from "../add-note-modal/add-note-modal";
+import { Note } from "../../models/note.model";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  notes: Note[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public modalWin: ModalController,
+    public navParams: NavParams
+  ) {}
 
+  ionViewDidLoad() {}
+
+  addNote() {
+    const modal = this.modalWin.create(AddNoteModalPage, null, {
+      enableBackdropDismiss: false
+    });
+
+    modal.present();
+
+    modal.onDidDismiss((data: any, role: string) => {
+      if (data) {
+        this.notes.unshift(data);
+      }
+    });
   }
-
 }
